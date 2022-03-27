1a12c9a20f6fa5dfa426b29e62ce6a3d92fe34bd
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-03-27 22:55:46 +0500

    Массивы

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,34 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    const numericData = JSON.parse('[' + data + ']');
+    numericData.sort((a, b) => a - b);
+    const result = numericData.toString();
+
+    return result;
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    const result = data.filter((element) => element < 100);
+    return result;
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let i = 0;
+    let resultArray = [];
+    for (i; i < array1.length; i++) {
+        resultArray = resultArray.concat(array1[i]).concat(array2[i]);
+    }
+    return resultArray;
+}
